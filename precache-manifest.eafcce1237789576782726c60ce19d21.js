2250c511c62ac61e1a473e8994976b405516002b
KenChen666 <z121356777@gmail.com>
2019-12-27 00:28:08 +0800

    deploy

---
+++ precache-manifest.eafcce1237789576782726c60ce19d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7459962b53a492356baf",
+    "url": "/KKBOXAPI-Demo/css/app.ccf1ac5f.css"
+  },
+  {
+    "revision": "0d7a5ef45f36325e5f1a",
+    "url": "/KKBOXAPI-Demo/css/chunk-vendors.7e449d15.css"
+  },
+  {
+    "revision": "4b39b778dcea9f379cf8a01ba482c005",
+    "url": "/KKBOXAPI-Demo/index.html"
+  },
+  {
+    "revision": "7459962b53a492356baf",
+    "url": "/KKBOXAPI-Demo/js/app.c641f7a0.js"
+  },
+  {
+    "revision": "0d7a5ef45f36325e5f1a",
+    "url": "/KKBOXAPI-Demo/js/chunk-vendors.99421a94.js"
+  },
+  {
+    "revision": "79e198297859b30d1389d35433222ec5",
+    "url": "/KKBOXAPI-Demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/KKBOXAPI-Demo/robots.txt"
+  }
+]);
